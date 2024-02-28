9f053081f8ba95534cc80940463dc7619874f1d3
Lorene15 <loreneterstena@gmail.com>
2024-02-28 12:33:14 -1100

    created form (input, button)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import React, { useState } from "react";
 import Typography from "@mui/joy/Typography";
+import Card from "@mui/joy/Card";
+import Button from "@mui/joy/Button";
+import Input from "@mui/joy/Input";
+import CardContent from "@mui/joy/CardContent";
+import Grid from "@mui/joy/Grid";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function handleAddTasks(task) {
+    setTasks((tasks) => [...tasks, task]);
+  }
+
   return (
-    <div>
-      <Header />
+    <div className="app">
+      <div className="container">
+        <Header />
+        <Form onHandleAddTasks={handleAddTasks} />
+      </div>
     </div>
   );
 }
@@ -15,6 +30,49 @@ function Header() {
         To Do List
       </Typography>
     </div>
+  );
+}
+
+function Form({ onHandleAddTasks }) {
+  const [tasks, setTasks] = useState("");
+
+  function handleTasks(e) {
+    setTasks(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTask = { id: Date.now(), tasks, packed: false };
+    if (!tasks) return;
+    onHandleAddTasks(newTask);
+    setTasks("");
+  }
+  return (
+    <Card sx={{ width: 600 }}>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+            <Grid xs={6} md={8}>
+              <Input
+                color="success"
+                placeholder="Add a task..."
+                type="text"
+                size="md"
+                variant="solid"
+                onChange={handleTasks}
+                value={tasks}
+              />
+            </Grid>
+            <Grid xs={6} md={4}>
+              <Button type="submit" variant="solid" size="md" color="success">
+                Add
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
 
