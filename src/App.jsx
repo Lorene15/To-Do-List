f6a2da2a50dab7589679eeb02fd872a5f1ed7701
Lorene15 <loreneterstena@gmail.com>
2024-02-28 11:04:47 -1100

    header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Typography from "@mui/joy/Typography";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <Typography color="success" level="h1" variant="plain">
+        To Do List
+      </Typography>
+    </div>
+  );
 }
 
 export default App;
