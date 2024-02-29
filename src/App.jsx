ef5bdfaf9cf5c446bc5f0705f5cec42ef41ba2b0
Lorene15 <loreneterstena@gmail.com>
2024-02-29 07:56:00 -1100

    Button  arrangement edit, delete, save, cancel

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,50 @@ import Button from "@mui/joy/Button";
 import Input from "@mui/joy/Input";
 import CardContent from "@mui/joy/CardContent";
 import Grid from "@mui/joy/Grid";
+import Checkbox from "@mui/joy/Checkbox";
+import Sheet from "@mui/joy/Sheet";
+import { Box } from "@mui/joy";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [items, setItems] = useState([]);
+  const [showList, setShowList] = useState(false);
 
-  function handleAddTasks(task) {
-    setTasks((tasks) => [...tasks, task]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+    setShowList(true);
+  }
+
+  function handleEditItem(itemId, editedItemText) {
+    setItems(
+      items.map((item) =>
+        item.id === itemId ? { ...item, tasks: editedItemText } : item
+      )
+    );
+  }
+
+  function handleDeleteItem(itemId) {
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete this task?"
+    );
+    if (isConfirmed) {
+      setItems(items.filter((item) => item.id !== itemId));
+    }
   }
 
   return (
     <div className="app">
       <div className="container">
         <Header />
-        <Form onHandleAddTasks={handleAddTasks} />
+        <Form onHandleAddItems={handleAddItems} setShowList={setShowList} />
+        {showList && (
+          <div className="spacer">
+            <ListTasks
+              items={items}
+              onDeleteItem={handleDeleteItem}
+              onEditItem={handleEditItem}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
@@ -33,27 +64,29 @@ function Header() {
   );
 }
 
-function Form({ onHandleAddTasks }) {
+function Form({ onHandleAddItems, setShowList }) {
   const [tasks, setTasks] = useState("");
 
   function handleTasks(e) {
+    e.preventDefault();
     setTasks(e.target.value);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    const newTask = { id: Date.now(), tasks, packed: false };
+    const newItem = { id: Date.now(), tasks, packed: false };
     if (!tasks) return;
-    onHandleAddTasks(newTask);
+    onHandleAddItems(newItem);
     setTasks("");
+    setShowList(true);
   }
   return (
     <Card sx={{ width: 600 }}>
       <CardContent>
         <form onSubmit={handleSubmit}>
           <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-            <Grid xs={6} md={8}>
+            <Grid xs={9}>
               <Input
                 color="success"
                 placeholder="Add a task..."
@@ -64,13 +97,126 @@ function Form({ onHandleAddTasks }) {
                 value={tasks}
               />
             </Grid>
-            <Grid xs={6} md={4}>
+            <Grid xs={3}>
               <Button type="submit" variant="solid" size="md" color="success">
                 Add
               </Button>
             </Grid>
           </Grid>
         </form>
+      </CardContent>
+    </Card>
+  );
+}
+
+function ListTasks({ items, onDeleteItem, onEditItem }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTaskText, setEditedTaskText] = useState("");
+  const [editedTaskId, setEditedTaskId] = useState("");
+
+  const handleEditClick = (taskId, taskText) => {
+    setIsEditing(true);
+    setEditedTaskText(taskText);
+    setEditedTaskId(taskId);
+  };
+
+  const handleSaveEdit = () => {
+    onEditItem(editedTaskId, editedTaskText);
+    setIsEditing(false);
+    setEditedTaskText("");
+    setEditedTaskId("");
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+    setEditedTaskText("");
+    setEditedTaskId("");
+  };
+
+  return (
+    <Card sx={{ width: 600 }}>
+      <CardContent>
+        <ul>
+          {items.map((item) => (
+            <Sheet key={item.id}>
+              <Grid
+                container
+                spacing={3}
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Grid xs={7}>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <Checkbox
+                      checked={item.packed}
+                      onChange={() => console.log("Checkbox changed")}
+                      color="success"
+                      variant="outlined"
+                    />
+                    <Grid xs={11}>
+                      <Input
+                        color="success"
+                        type="text"
+                        size="md"
+                        variant="solid"
+                        value={
+                          isEditing && editedTaskId === item.id
+                            ? editedTaskText
+                            : item.tasks
+                        }
+                        onChange={(e) => setEditedTaskText(e.target.value)}
+                        readOnly={!isEditing || editedTaskId !== item.id}
+                      />
+                    </Grid>
+                  </div>
+                </Grid>
+                <Grid xs={5}>
+                  {isEditing && editedTaskId === item.id ? (
+                    <div style={{ display: "flex" }}>
+                      <Button
+                        variant="plain"
+                        size="md"
+                        color="success"
+                        onClick={handleSaveEdit}
+                      >
+                        Save✔️
+                      </Button>
+                      <span style={{ margin: "0 5px" }}></span>
+                      <Button
+                        variant="plain"
+                        size="md"
+                        color="success"
+                        onClick={handleCancelEdit}
+                      >
+                        Cancel🚫
+                      </Button>
+                    </div>
+                  ) : (
+                    <div style={{ display: "flex" }}>
+                      <Button
+                        variant="plain"
+                        size="md"
+                        color="success"
+                        onClick={() => handleEditClick(item.id, item.tasks)}
+                      >
+                        Edit✏️
+                      </Button>
+                      <span style={{ margin: "0 5px" }}></span>
+                      <Button
+                        variant="plain"
+                        size="md"
+                        color="success"
+                        onClick={() => onDeleteItem(item.id)}
+                      >
+                        Delete❌
+                      </Button>
+                    </div>
+                  )}
+                </Grid>
+              </Grid>
+            </Sheet>
+          ))}
+        </ul>
       </CardContent>
     </Card>
   );
